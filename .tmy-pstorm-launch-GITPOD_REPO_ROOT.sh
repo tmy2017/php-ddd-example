be31b715585a8900bf23c852633f944a8f948560
Tmy2017 <tommykao2013@gmail.com>
2021-12-12 12:35:39 +0000

    update to newer image & add .tmy-pstorm-launch-GITPOD_REPO_ROOT to image

---
+++ .tmy-pstorm-launch-GITPOD_REPO_ROOT.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#projector run - learn from https://github.com/shaal/DrupalPod/blob/main/.gitpod/utils/phpstorm.template.sh
+~/.projector/configs/PhpStormByIdeAutoinstall-2021.2/run.sh ${GITPOD_REPO_ROOT}
