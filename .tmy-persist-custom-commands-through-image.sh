fa948436771864062190cce6a41100b3ca5de760
Tmy2017 <tommykao2013@gmail.com>
2021-12-13 09:38:22 +0000

    update ide shortcut, update image version, add new "persist custom commands" script

--- .tmy-persist-custom-commands-through-image.sh
+++ .tmy-persist-custom-commands-through-image.sh
@@ -1,21 +1,13 @@
-#!/bin/sh
 EPOCH_TIME=`date +%s`
 # get latest (there is a gotcha! see below) image
 #   NOTE: latest tag is dangerous, can easily overwrite local latest! https://github.com/moby/moby/issues/10291
 docker pull tmy2017/gitpod-pm
-docker run --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm /bin/sh -c "mkdir -p .local/share/JetBrains"
-# NOTE: docker cp has no glob pattern! https://github.com/moby/moby/issues/7710
-# NOTICE: copy files and directories inherently looks different - directories is whole folder
-#	hence second parameter looks asymmetric
-#	can also use /. in the second parameter, but feel very specific to docker hence not using
-#	Official info https://docs.docker.com/engine/reference/commandline/cp/#extended-description
-# NOTICE2: also seems not able to use tilde - must use absolute path to refer home path
-docker cp /home/gitpod/.projector/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/
-docker cp /home/gitpod/.config/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.config/
-docker cp /home/gitpod/.local/share/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.local/share/
+# use this zzdocker zzrun zzMinus-zzDash-zzd to zzdetach so zzLater you can still zzdocker zzexec zzMinus-zzDash-zzit to zzDebug and zzobserve!
+docker run -d -it --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm bash
+docker cp .tmy-persist-ide-settings-through-image-for-gitpod.sh  temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-persist-ide-settings-through-image-for-gitpod
+docker cp .tmy-persist-custom-commands-through-image.sh  temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-persist-custom-commands-through-image
 docker commit temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm
 docker tag tmy2017/gitpod-pm tmy2017/gitpod-pm:ver-${EPOCH_TIME}
-docker login
 docker push tmy2017/gitpod-pm:ver-${EPOCH_TIME}
 # also remember to update the latest! so that next time when docker pull latest really meaning latest
 #   TODO: but better have a "moving-tag" instead of default "latest", since it is dangerous and confusing...
