dbe62e6aa21692bad862a0c451ae5f046726e15e
nh2018gc <93929846+nh2018gc@users.noreply.github.com>
2021-12-18 22:41:10 +0100

    update persist ide setting command

--- .tmy-persist-ide-settings-through-image-for-gitpod.sh
+++ .tmy-persist-ide-settings-through-image-for-gitpod.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 EPOCH_TIME=`date +%s`
 # get latest (there is a gotcha! see below) image
 #   NOTE: latest tag is dangerous, can easily overwrite local latest! https://github.com/moby/moby/issues/10291
@@ -6,13 +5,15 @@ docker pull tmy2017/gitpod-pm
 docker run --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm /bin/sh -c "mkdir -p .local/share/JetBrains"
 # NOTE: docker cp has no glob pattern! https://github.com/moby/moby/issues/7710
 # NOTICE: copy files and directories inherently looks different - directories is whole folder
-#	hence second parameter looks asymmetric
-#	can also use /. in the second parameter, but feel very specific to docker hence not using
-#	Official info https://docs.docker.com/engine/reference/commandline/cp/#extended-description
+#   hence second parameter looks asymmetric
+#   can also use /. in the second parameter, but feel very specific to docker hence not using
+#   Official info https://docs.docker.com/engine/reference/commandline/cp/#extended-description
 # NOTICE2: also seems not able to use tilde - must use absolute path to refer home path
 docker cp /home/gitpod/.projector/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/
 docker cp /home/gitpod/.config/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.config/
 docker cp /home/gitpod/.local/share/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.local/share/
+# to skip already agreed license agreement
+docker cp /home/gitpod/.java temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.java
 docker commit temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm
 docker tag tmy2017/gitpod-pm tmy2017/gitpod-pm:ver-${EPOCH_TIME}
 docker login
