57f9b22303895ebb4f83788a9e0ea81ce29c676a
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2021-12-29 16:19:47 +0100

    (fix) update custom command file name and the coressponding .sh file (fix) missing container name

--- .tmy-update-custom-cmds-from-usr-local-bin.sh
+++ .tmy-update-custom-cmds-from-usr-local-bin.sh
@@ -1,4 +1,3 @@
-echo "!!!MUST have .tmy-xyz files in current directory, if not then this command is not useful!!!"
 EPOCH_TIME=`date +%s`
 # get latest (there is a gotcha! see below) image
 #   NOTE: latest tag is dangerous, can easily overwrite local latest! https://github.com/moby/moby/issues/10291
@@ -6,12 +5,12 @@ docker pull tmy2017/gitpod-pm
 # use this zzdocker zzrun zzMinus-zzDash-zzd to zzdetach so zzLater you can still zzdocker zzexec zzMinus-zzDash-zzit to zzDebug and zzobserve!
 docker run -d -it --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm bash
 # directly copy what is inside CURRENT environment's updated custom command, so not repo (ex: tmy2017/php-ddd-example) specific 
-docker cp /usr/local/bin/tmy-persist-ide-settings-through-image-for-gitpod  temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-persist-ide-settings-through-image-for-gitpod
-docker cp /usr/local/bin/tmy-persist-custom-commands-through-image temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-persist-custom-commands-through-image
+docker cp /usr/local/bin/tmy-persist-ide-settings-through-image-for-gitpod temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-persist-ide-settings-through-image-for-gitpod
+docker cp /usr/local/bin/tmy-update-custom-cmds-from-usr-local-bin temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-update-custom-cmds-from-usr-local-bin
 # also update the pstorm launch command for disabling ORG_JETBRAINS_PROJECTOR_SERVER_AUTO_KEYMAP
 #   see https://youtrack.jetbrains.com/issue/PRJ-121#focus=Comments-27-5198388.0-0 and
 #   https://jetbrains.github.io/projector-client/mkdocs/latest/ij_user_guide/server_customization/#list-of-parameters  
-docker cp /usr/local/bin/tmy-pstorm-launch-GITPOD_REPO_ROOT ${EPOCH_TIME}:/usr/local/bin/tmy-pstorm-launch-GITPOD_REPO_ROOT
+docker cp /usr/local/bin/tmy-pstorm-launch-GITPOD_REPO_ROOT temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-pstorm-launch-GITPOD_REPO_ROOT
 docker commit temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm
 docker tag tmy2017/gitpod-pm tmy2017/gitpod-pm:ver-${EPOCH_TIME}
 docker push tmy2017/gitpod-pm:ver-${EPOCH_TIME}
