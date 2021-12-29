1861aaa9b6013ae79b98727d8964588e2700cb6f
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2021-12-29 13:07:18 +0100

    improve: After phpstorm trial expires, will start using real license info. Hence  persist ide setting command needs to exclude license related files

--- .tmy-persist-ide-settings-through-image-for-gitpod.sh
+++ .tmy-persist-ide-settings-through-image-for-gitpod.sh
@@ -1,26 +1,48 @@
+#!/bin/sh
 EPOCH_TIME=`date +%s`
 # get latest (there is a gotcha! see below) image
 #   NOTE: latest tag is dangerous, can easily overwrite local latest! https://github.com/moby/moby/issues/10291
 docker pull tmy2017/gitpod-pm
-docker run --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm /bin/sh -c "mkdir -p ~/.local/share/JetBrains; mkdir -p ~/.java/.userPrefs/jetbrains"
+docker run --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm /bin/sh -c "mkdir -p .local/share/JetBrains"
+
 # NOTE: docker cp has no glob pattern! https://github.com/moby/moby/issues/7710
 # NOTICE: copy files and directories inherently looks different - directories is whole folder
-#   hence second parameter looks asymmetric
-#   can also use /. in the second parameter, but feel very specific to docker hence not using
-#   Official info https://docs.docker.com/engine/reference/commandline/cp/#extended-description
+#	hence second parameter looks asymmetric
+#	can also use /. in the second parameter, but feel very specific to docker hence not using
+#	Official info https://docs.docker.com/engine/reference/commandline/cp/#extended-description
 # NOTICE2: also seems not able to use tilde - must use absolute path to refer home path
 docker cp /home/gitpod/.projector/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/
 docker cp /home/gitpod/.config/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.config/
 docker cp /home/gitpod/.local/share/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.local/share/
-# to skip already agreed license agreement
-docker cp /home/gitpod/.java/.userPrefs/jetbrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.java/.userPrefs/jetbrains/
-docker commit temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm
-docker tag tmy2017/gitpod-pm tmy2017/gitpod-pm:ver-${EPOCH_TIME}
+
+### License related - START
+# Now trial expires, start to provide real license, but still need to persist IDE settings hence need the following
+
+# do NOT persist .java since after trial expires and real license provided, this could contain license info
+#   so to skip the license agreement
+#   docker cp /home/gitpod/.java temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.java
+
+# delete license related info in .config folder
+#   use -f to skip error
+docker run --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm /bin/sh -c "rm -f /home/gitpod/.config/JetBrains/PhpStorm2021.2/phpstorm.key"
+
+### License related - END
+
+### Push to repo -START
+# Use specific tag so to avoid gitpod caching docker registry image
 docker login
+docker tag tmy2017/gitpod-pm tmy2017/gitpod-pm:ver-${EPOCH_TIME}
 docker push tmy2017/gitpod-pm:ver-${EPOCH_TIME}
-# also remember to update the latest! so that next time when docker pull latest really meaning latest
+
+# Remember to update the latest! 
+
+# so that next time when docker pull latest really meaning latest
 #   TODO: but better have a "moving-tag" instead of default "latest", since it is dangerous and confusing...
 #   https://vsupalov.com/docker-latest-tag/, https://www.cloudsavvyit.com/10691/understanding-dockers-latest-tag/
+docker commit temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm
 docker push tmy2017/gitpod-pm:latest
+
+### Push to repo - END
+
 echo "success - the newest image name with tag is: tmy2017/gitpod-pm:ver-${EPOCH_TIME}"
 echo "please remember to update .gitpod.yml image name to ensure it has latest image"
