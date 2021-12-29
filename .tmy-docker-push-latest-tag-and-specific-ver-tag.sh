de1a60637d5daa8a9f4bd4bf624469296bea2817
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2021-12-29 17:36:02 +0100

    (chore) document command to build php image

--- .tmy-docker-push-latest-tag-and-specific-ver-tag.sh
+++ .tmy-docker-push-latest-tag-and-specific-ver-tag.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 EPOCH_TIME=`date +%s`
-
+# NOTE: command to build the dockerfile as reference
+#   docker build -f .gitpod.gitpod-php71-mysql8-pstorm.Dockerfile -t tmy2017/gitpod-php71-mysql8-pstorm .
 # TODO: make it dynamic by receiving command line argument
 docker tag tmy2017/gitpod-php71-mysql8-pstorm tmy2017/gitpod-php71-mysql8-pstorm:ver-${EPOCH_TIME}
 docker push tmy2017/gitpod-php71-mysql8-pstorm:latest
