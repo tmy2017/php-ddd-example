9a4632a157396a835c4c0778c3c6f71d5c733c76
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2021-12-29 14:38:21 +0100

    (fix) directly copy what is inside CURRENT environment's updated custom command, so not repo (ex: tmy2017/php-ddd-example) specific

--- .tmy-update-custom-cmds-from-usr-local-bin.sh
+++ .tmy-update-custom-cmds-from-usr-local-bin.sh
@@ -5,12 +5,13 @@ EPOCH_TIME=`date +%s`
 docker pull tmy2017/gitpod-pm
 # use this zzdocker zzrun zzMinus-zzDash-zzd to zzdetach so zzLater you can still zzdocker zzexec zzMinus-zzDash-zzit to zzDebug and zzobserve!
 docker run -d -it --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm bash
-docker cp .tmy-persist-ide-settings-through-image-for-gitpod.sh  temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-persist-ide-settings-through-image-for-gitpod
-docker cp .tmy-persist-custom-commands-through-image.sh  temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-persist-custom-commands-through-image
+# directly copy what is inside CURRENT environment's updated custom command, so not repo (ex: tmy2017/php-ddd-example) specific 
+docker cp /usr/local/bin/tmy-persist-ide-settings-through-image-for-gitpod  temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-persist-ide-settings-through-image-for-gitpod
+docker cp /usr/local/bin/tmy-persist-custom-commands-through-image temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-persist-custom-commands-through-image
 # also update the pstorm launch command for disabling ORG_JETBRAINS_PROJECTOR_SERVER_AUTO_KEYMAP
 #   see https://youtrack.jetbrains.com/issue/PRJ-121#focus=Comments-27-5198388.0-0 and
 #   https://jetbrains.github.io/projector-client/mkdocs/latest/ij_user_guide/server_customization/#list-of-parameters  
-docker cp .tmy-pstorm-launch-GITPOD_REPO_ROOT.sh ${EPOCH_TIME}:/usr/local/bin/tmy-pstorm-launch-GITPOD_REPO_ROOT
+docker cp /usr/local/bin/tmy-pstorm-launch-GITPOD_REPO_ROOT ${EPOCH_TIME}:/usr/local/bin/tmy-pstorm-launch-GITPOD_REPO_ROOT
 docker commit temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-pm
 docker tag tmy2017/gitpod-pm tmy2017/gitpod-pm:ver-${EPOCH_TIME}
 docker push tmy2017/gitpod-pm:ver-${EPOCH_TIME}
