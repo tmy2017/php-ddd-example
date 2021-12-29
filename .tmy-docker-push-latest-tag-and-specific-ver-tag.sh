da164b2c4d3cdc6ca9b36d9464dbd0e420e1db45
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2021-12-29 13:25:19 +0100

    (improve) specific tag should be the last in command so it can be copy and paste easily

--- .tmy-docker-push-latest-tag-and-specific-ver-tag.sh
+++ .tmy-docker-push-latest-tag-and-specific-ver-tag.sh
@@ -3,5 +3,5 @@ EPOCH_TIME=`date +%s`
 
 # TODO: make it dynamic by receiving command line argument
 docker tag tmy2017/gitpod-php71-mysql8-pstorm tmy2017/gitpod-php71-mysql8-pstorm:ver-${EPOCH_TIME}
-docker push tmy2017/gitpod-php71-mysql8-pstorm:ver-${EPOCH_TIME}
 docker push tmy2017/gitpod-php71-mysql8-pstorm:latest
+docker push tmy2017/gitpod-php71-mysql8-pstorm:ver-${EPOCH_TIME}
