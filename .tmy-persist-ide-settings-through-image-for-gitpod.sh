e85e95e769fc5ad91ed331f2207a56b2e5ab6000
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2021-12-30 07:37:27 +0100

    (fix)  !!! nasty-bug! MUST delete the mysqld.pid BEFORE docker commit, so mysqld next time when docker run will auto start again

--- .tmy-persist-ide-settings-through-image-for-gitpod.sh
+++ .tmy-persist-ide-settings-through-image-for-gitpod.sh
@@ -12,15 +12,17 @@ docker run -d -it --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-psto
 docker exec temp-for-image-commit-${EPOCH_TIME} /bin/sh -c "mkdir -p .local/share/JetBrains"
 
 # NOTE: docker cp has no glob pattern! https://github.com/moby/moby/issues/7710
-# NOTICE: copy files and directories inherently looks different - directories is whole folder
+# NOTICE: 
+# 1) copy files and directories inherently looks different - directories is whole folder
 #	hence second parameter looks asymmetric
 #	can also use /. in the second parameter, but feel very specific to docker hence not using
 #	Official info https://docs.docker.com/engine/reference/commandline/cp/#extended-description
-# NOTICE2: also seems not able to use tilde - must use absolute path to refer home path
+# 2) also seems not able to use tilde - must use absolute path to refer home path
 docker cp /home/gitpod/.projector/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/
 docker cp /home/gitpod/.config/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.config/
 docker cp /home/gitpod/.local/share/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.local/share/
-
+# 3) !!! nasty-bug! MUST delete the mysqld.pid BEFORE docker commit, so mysqld next time when docker run will auto start again
+docker exec temp-for-image-commit-${EPOCH_TIME} /bin/sh -c "rm -rf /var/run/mysqld/mysqld.pid"
 ### License related - START
 # Now trial expires, start to provide real license, but still need to persist IDE settings hence need the following
 
