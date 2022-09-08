94b6119c36823442ac9c643df997c6cef8e65b4c
Tommy OnyxOne <60062682+TmEuMail-2020@users.noreply.github.com>
2022-09-09 00:38:49 +0200

    (fix) delete (reset) /var/run/tmy-phpstorm-bookmark-sync.pid to ensure bookmark sync will work

--- .tmy-save-ide-settings-through-image-for-gitpod.sh
+++ .tmy-save-ide-settings-through-image-for-gitpod.sh
@@ -12,21 +12,30 @@ docker run -d -it --name temp-for-image-commit-${EPOCH_TIME} tmy2017/gitpod-psto
 docker exec temp-for-image-commit-${EPOCH_TIME} /bin/sh -c "mkdir -p .local/share/JetBrains"
 
 # fix seems cache folder also needs to be included - to fix "mysteriously disappeared plugin" (and settings) (11-Apr-2022T10-37+0200)
-#	https://intellij-support.jetbrains.com/hc/en-us/articles/206544519
+#       https://intellij-support.jetbrains.com/hc/en-us/articles/206544519
 docker cp /home/gitpod/.cache/JetBrains/PhpStorm2021.3 temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.cache/JetBrains/
 
 # NOTE: docker cp has no glob pattern! https://github.com/moby/moby/issues/7710
 # NOTICE: 
 # 1) copy files and directories inherently looks different - directories is whole folder
-#	hence second parameter looks asymmetric
-#	can also use /. in the second parameter, but feel very specific to docker hence not using
-#	Official info https://docs.docker.com/engine/reference/commandline/cp/#extended-description
+#       hence second parameter looks asymmetric
+#       can also use /. in the second parameter, but feel very specific to docker hence not using
+#       Official info https://docs.docker.com/engine/reference/commandline/cp/#extended-description
 # 2) also seems not able to use tilde - must use absolute path to refer home path
 docker cp /home/gitpod/.projector/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/
 docker cp /home/gitpod/.config/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.config/
 docker cp /home/gitpod/.local/share/JetBrains/ temp-for-image-commit-${EPOCH_TIME}:/home/gitpod/.local/share/
+
+#also copy save command, sometimes also updated zz-_36-_y22-0909-003
+docker cp /usr/local/bin/tmy-save-ide-settings-through-image-for-gitpod temp-for-image-commit-${EPOCH_TIME}:/usr/local/bin/tmy-save-ide-settings-through-image-for-gitpod
+
 # 3) !!! nasty-bug! MUST delete the mysqld.pid BEFORE docker commit, so mysqld next time when docker run will auto start again
-docker exec temp-for-image-commit-${EPOCH_TIME} /bin/sh -c "rm -rf /var/run/mysqld/mysqld.pid"
+# use SUDO! so to avoid "permission denied" error from /var/run zz-_36-_y22-0909-0028
+docker exec temp-for-image-commit-${EPOCH_TIME} /bin/sh -c "sudo rm -rf /var/run/mysqld/mysqld.pid"
+# 4) the same for bookmark sync pid - reset for sanity zz-_36-_y22-0909-0011
+docker exec temp-for-image-commit-${EPOCH_TIME} /bin/sh -c "sudo rm -rf /var/run/tmy-phpstorm-bookmark-sync.pid"
+# delete things in bookmark folder to reset phpstorm bookmark folder for sanity 
+docker exec temp-for-image-commit-${EPOCH_TIME} /bin/sh -c "sudo rm -rf /home/gitpod/.config/JetBrains/PhpStorm2021.3/workspace/*"
 ### License related - START
 # Now trial expires, start to provide real license, but still need to persist IDE settings hence need the following
 
